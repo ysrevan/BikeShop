d479b9f02b547571d7b57f52a9c73e593c304bae
ysrevan <yusiflirvan921@gmail.com>
2025-06-21 00:31:37 +0400

    aboutpage style

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Ride from '../components/ride/Ride'
+import Aboutpage from '../components/pageheader/Aboutpage'
+import Aboutexperience from '../components/aboutexprience/Aboutexperience'
+import Abouthumble from '../components/abouthumble/Abouthumble'
 
 function About() {
   return (
     <>
+    <Aboutpage/>
+    <Aboutexperience/>
      <Ride/>
+     <Abouthumble/>
     </>
   )
 }
