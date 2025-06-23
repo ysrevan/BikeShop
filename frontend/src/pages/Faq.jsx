5fecf4a3a8ec4bf899a737ff791df42526f3050d
ysrevan <yusiflirvan921@gmail.com>
2025-06-24 00:08:08 +0400

    faq style

--- frontend/src/pages/Faq.jsx
+++ frontend/src/pages/Faq.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Accordion from '../components/accordion/Accordion'
 
 function Faq() {
   return (
-    <div>Faq</div>
+    <>
+    <Accordion/>
+    </>
   )
 }
 
