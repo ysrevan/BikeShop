50ee7bf4bdc50cab85fd6ac03c8c8fc6636b1430
ysrevan <yusiflirvan921@gmail.com>
2025-06-25 00:40:06 +0400

    shop and blog mongodb add

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,15 @@
 import express from "express"
 import "dotenv/config"
 import "./src/db/Connection.js"
+import productRouter from "./src/routes/ProductRouter.js"
+import blogRouter from "./src/routes/BlogRouter.js"
 const app = express()
 const port = process.env.PORT || 5001
+
+app.use(express.json()); 
+app.use("/api/products", productRouter);
+app.use("/api/blogs/",blogRouter)
+app.use("/images", express.static("src/images"));
 
 app.listen(port,()=>{
     console.log(`Server is running ${`http://localhost:${port}`}`);
--- backend/src/routes/ProductRouter.js
+++ backend/src/routes/ProductRouter.js
@@ -1,11 +1,57 @@
-import express from "express"
-import { addProduct, deleteProduct, getProducts } from "../controllers/ProductController.js";
+// import express from "express"
+// import { addProduct, deleteProduct, getProducts } from "../controllers/ProductController.js";
 
-const productRouter =  express.Router()
+// const productRouter =  express.Router()
+
+
+// productRouter.post("/", upload.single("image"), addProduct);
+// productRouter.get("/", getProducts);
+// productRouter.delete("/:id", deleteProduct);
+
+// export default productRouter
+
+// import express from "express"
+// import { addProduct, deleteProduct, getProducts } from "../controllers/ProductController.js";
+// import multer from "multer"; 
+
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'public/images'); 
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + '-' + file.originalname); 
+//   }
+// });
+
+// const upload = multer({ storage }); 
+
+// const productRouter = express.Router();
+
+// productRouter.post("/", upload.single("image"), addProduct);
+// productRouter.get("/", getProducts);
+// productRouter.delete("/:id", deleteProduct);
+
+// export default productRouter;
+
+
+import express from "express";
+
+
+import {
+  addProduct,
+  deleteProduct,
+  getProducts,
+} from "../controllers/productController.js";
+import upload from "../upload/upload.js";
+
+const productRouter = express.Router();
+
 
 
 productRouter.post("/", upload.single("image"), addProduct);
 productRouter.get("/", getProducts);
 productRouter.delete("/:id", deleteProduct);
 
-export default productRouter
+
+export default productRouter;
